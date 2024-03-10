123662b17b8fd4984fb079c32d0fc18b91e2f9e9
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-03-10 12:23:41 -0300

    Adicionando o projeto, ainda falta testar mas por agora é isto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# criar-resumos-de-pdf
+# Criar resumos de artigos em PDF
+
+Este projeto utiliza a biblioteca Langchain para resumir artigos em PDF. O código define duas funções principais: `resumir_texto` e `executar_programa`. A primeira função recebe um texto como entrada e retorna um resumo desse texto. A segunda função processa uma lista de endereços de arquivos PDF, extrai o texto de cada PDF, resumindo-o e salvando o resultado em um arquivo TXT. O projeto também define uma classe `Artigo` que representa um artigo em PDF, com atributos como endereço, título, texto e resumo.
---
+++ main.py
@@ -0,0 +1,69 @@
+from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import PyPDFLoader
+from langchain_openai import ChatOpenAI
+from dataclasses import dataclass
+from dotenv import load_dotenv
+import glob, os, time
+
+load_dotenv()
+llm = ChatOpenAI(
+    temperature=0,
+    model_name=os.getenv("MODEL_NAME"),
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
+
+
+def resumir_texto(texto: str) -> str:
+    chain = load_summarize_chain(llm, chain_type="map_reduce")
+    summary = chain.run(texto)
+
+    return summary
+
+
+def executar_programa(enderecos_pdf: list[str]) -> None:
+    artigos = []
+    for i in enderecos_pdf:
+        print(f"Processando {a.titulo}...")
+
+        a = Artigo(i)
+
+        artigos.append(a)
+
+        with open(f"resumos/{a.titulo}.txt", "w") as f:
+            f.write(
+                f"""
+                    TITULO:
+                    
+                    {a.titulo}
+                    
+                    TEXTO:
+                    
+                    {a.texto}
+                    
+                    
+                    RESUMO:
+                    
+                    {a.resumo}
+                    
+                    """
+            )
+
+
+@dataclass
+class Artigo:
+    endereco: str
+    titulo: str | None = None
+    texto: str | None = None
+    resumo: str | None = None
+
+    def __post_init__(self) -> None:
+        self.titulo = os.path.basename(self.endereco).replace(".pdf", "")
+        loader = PyPDFLoader(self.endereco)
+        self.texto = loader.load_and_split()
+        self.resumo = resumir_texto(self.texto)
+
+
+if __name__ == "__main__":
+    enderecos_pdf = glob.glob("pdfs/*.pdf")
+
+    executar_programa(enderecos_pdf)
